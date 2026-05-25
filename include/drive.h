458dd4f81a778329ea5c565c397baafc55c2f461
Veronica Yarovinsky <77183107+veronicayarovinsky@users.noreply.github.com>
2026-05-25 14:52:26 -0400

    Update drive.h

--- include/drive.h
+++ include/drive.h
@@ -1,12 +1,23 @@
-#ifndef FSM_H
-#define FSM_H
+/**
+ * @file drive.h
+ * @brief basic actions; movement through maze consists of a sequence of these actions, called by the FSM in main.
+ */
+
+#ifndef DRIVE_H
+#define DRIVE_H
 
 #include <Arduino.h>
 
+/* initializes controllers */
 void drive_init();
+
+/* zeros motors & resets all controllers */
 void drive_stop();
 
-bool drive_forward(float distance_m, float speed_m_s);
+/* drives forward for a fixed distance */
+bool drive_forward(float distance_mm, float speed_mm_s);
+
+/* spins in place by delta_heading_rad (positive = left) */
 bool drive_turn(float delta_heading_rad);
 
 #endif
