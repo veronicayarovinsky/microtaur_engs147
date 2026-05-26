ffb63f99aa1434bf24004b4da62d07a0ecb8346c
Veronica Yarovinsky <77183107+veronicayarovinsky@users.noreply.github.com>
2026-05-26 11:10:49 -0400

    Update tof.h

--- include/tof.h
+++ include/tof.h
@@ -2,6 +2,13 @@
  * @file tof.h
  * @brief gets distance readings from 3x VL53L4CD TOF sensors thru PCA9548 mux
  *        & writes --> Micromouse::tof (distances and wall booleans)
+ * 
+ * Using polling mode
+ * 
+ * Datasheet: https://www.st.com/resource/en/datasheet/vl53l4cd.pdf
+ * User Manual for API / functions: https://www.st.com/resource/en/user_manual/um2931-a-guide-to-using-the-vl53l4cd-ultra-lite-driver-uld-stmicroelectronics.pdf
+ * Product Page: https://www.st.com/en/imaging-and-photonics-solutions/vl53l4cd.html?ecmp=tt9470_gl_link_feb2019&rt=ds&id=DS13812#documentation
+ * Arduino Lib Github: https://github.com/stm32duino/VL53L4CD
  */
 
 #ifndef TOF_H
